a4af55d42fbd3b8a367fa47fa137cdcd1a40bfbc
Sabina Bajracharya <sabinabajracharya42@gmail.com>
2026-02-13 10:23:20 +0545

    Update NotesPage.java: Changed the element to be discoverable by id to by xpath.

--- MyNoteAutomation/src/test/java/pages/NotesPage.java
+++ MyNoteAutomation/src/test/java/pages/NotesPage.java
@@ -9,24 +9,27 @@ public class NotesPage {
     public NotesPage(WebDriver driver) {
         this.driver = driver;
     }
-
+    
+    //To create a new note.
     public void createNote(String title, String description) {
         driver.findElement(By.xpath("//button[text()='Add Note']")).click();
-        driver.findElement(By.id("title")).sendKeys(title);
-        driver.findElement(By.id("description")).sendKeys(description);
-        driver.findElement(By.xpath("//button[text()='Create']")).click();
+        driver.findElement(By.xpath("//input[@id='title']")).sendKeys(title);
+        driver.findElement(By.xpath("//textarea[@id='description']")).sendKeys(description);
+        driver.findElement(By.xpath("//button[normalize-space()='Create']")).click();
     }
-
+    
+    //To edit an existing note.
     public void editNote(String newTitle) {
-        driver.findElement(By.xpath("//button[text()='Edit']")).click();
-        WebElement title = driver.findElement(By.id("title"));
+        driver.findElement(By.xpath("//button[normalize-space()='Edit']")).click();
+        WebElement title = driver.findElement(By.xpath("//input[@id='title']"));
         title.clear();
-        title.sendKeys(newTitle);
-        driver.findElement(By.xpath("//button[text()='Update']")).click();
+        title.sendKeys("newTitle");
+        driver.findElement(By.xpath("//button[normalize-space()='Save']")).click();
     }
-
+    
+    //To delete an existing note.
     public void deleteNote() {
-        driver.findElement(By.xpath("//button[text()='Delete']")).click();
-        driver.findElement(By.xpath("//button[text()='Delete']")).click();
+        driver.findElement(By.xpath("//button[normalize-space()='Delete']")).click();
+        driver.findElement(By.xpath("//button[@type='button'][normalize-space()='Delete']")).click();
     }
 }
